981944ea8f55cfc8d89f1260443d53d8b6c84f9e
EF <ef@idaproject.com>
2018-03-28 16:10:55 +0300

    Fix main.js

--- js/main.js
+++ js/main.js
@@ -1,48 +1,53 @@
-const SCREEN_TEMPLATES = [
-  document.querySelector(`#greeting`).content,
-  document.querySelector(`#rules`).content,
-  document.querySelector(`#game-1`).content,
-  document.querySelector(`#game-2`).content,
-  document.querySelector(`#game-3`).content,
-  document.querySelector(`#stats`).content,
-];
-
 const RIGHT_ARROW_CODE = 39;
 const LEFT_ARROW_CODE = 37;
 
+const SCREEN_TEMPLATE_CLASS = '.js-screen-template';
+
+const MIN_TEMPLATE_NUMBER = 1;
+const TEMPLATE_NUMBER = document.querySelectorAll(SCREEN_TEMPLATE_CLASS).length;
+const INITIAL_SCREEN_INDEX = 1;
+
 const mainElement = document.querySelector(`.js-main`);
 
-let currentScreenIndex = null;
+const screenTemplates = [];
+
+let currentScreenIndex = 0;
+
+const createScreenArray = () => {
+  for (let i = MIN_TEMPLATE_NUMBER; i <= TEMPLATE_NUMBER; i++) {
+    screenTemplates.push(document.querySelector(`${SCREEN_TEMPLATE_CLASS}[data-index="${i}"`).content);
+  }
+};
 
 const showScreen = (screenIndex) => {
   currentScreenIndex = screenIndex;
 
   mainElement.innerHTML = ``;
-  mainElement.appendChild(SCREEN_TEMPLATES[currentScreenIndex - 1].cloneNode(true));
+  mainElement.appendChild(screenTemplates[currentScreenIndex - 1].cloneNode(true));
 };
 
-const checkScreenIndex = (index) => {
-  return index >= 1 && index <= SCREEN_TEMPLATES.length;
+const addScreenSwitch = () => {
+  document.addEventListener(`keydown`, (event) => {
+    if (event.altKey) {
+      if (event.keyCode === RIGHT_ARROW_CODE) {
+        let screenIndex = currentScreenIndex + 1;
+
+        if (screenIndex <= TEMPLATE_NUMBER) {
+          showScreen(screenIndex);
+        }
+      }
+
+      if (event.keyCode === LEFT_ARROW_CODE) {
+        let screenIndex = currentScreenIndex - 1;
+
+        if (screenIndex >= MIN_TEMPLATE_NUMBER) {
+          showScreen(screenIndex);
+        }
+      }
+    }
+  });
 };
 
-document.addEventListener('keydown', function (event) {
-  if (event.altKey) {
-    if (event.keyCode === RIGHT_ARROW_CODE) {
-      let screenIndex = currentScreenIndex + 1;
-
-      if (checkScreenIndex(screenIndex)) {
-        showScreen(screenIndex);
-      }
-    }
-
-    if (event.keyCode === LEFT_ARROW_CODE) {
-      let screenIndex = currentScreenIndex - 1;
-
-      if (checkScreenIndex(screenIndex)) {
-        showScreen(screenIndex);
-      }
-    }
-  }
-});
-
-showScreen(3);
+createScreenArray();
+showScreen(INITIAL_SCREEN_INDEX);
+addScreenSwitch();
